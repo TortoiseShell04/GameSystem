1eb23cfe0993e2ea2683ac1a3780836ccddf52a2
Bruh <bruh@kali.Kali>
2022-11-19 16:08:26 +0200

    ./

---
+++ RiderProjects/ConsoleApp3/GameSystem/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GameSystem
+{
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            WeaponManager.sort();
+            EnemyManager.enemyManager();
+            ArmorManager.select();
+            PlayerStats.PlayerRun();
+        }
+    }
+}
